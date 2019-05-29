64933a1e95c9125a21a2bd9b45d135bdefafea18
techednelson <techednelson@gmail.com>
2019-05-29 21:37:45 +0300

    Updates

---
+++ precache-manifest.4b65f4353db34ef52c0ad07b69617422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bee795d2cea97b1f44ad6fbb3653650",
+    "url": "/robofriends-react-redux/index.html"
+  },
+  {
+    "revision": "fe69bb0556650eebb72f",
+    "url": "/robofriends-react-redux/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e4f07270de8ee8c89e6a",
+    "url": "/robofriends-react-redux/static/css/main.4347c129.chunk.css"
+  },
+  {
+    "revision": "fe69bb0556650eebb72f",
+    "url": "/robofriends-react-redux/static/js/2.1c01544c.chunk.js"
+  },
+  {
+    "revision": "e4f07270de8ee8c89e6a",
+    "url": "/robofriends-react-redux/static/js/main.931baf8c.chunk.js"
+  },
+  {
+    "revision": "e9b2bb423f06c83ccfd5",
+    "url": "/robofriends-react-redux/static/js/runtime~main.a64ce416.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
