3f9d0140b664348f066465b35cb2e4f87148f118
techednelson <techednelson@gmail.com>
2019-05-29 23:22:57 +0300

    fix: change colors in order to improve score for lighthouse

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const card = ({ name, email, id }) => {
     return (
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <div className='tc bg-card dib br3 pa3 ma2 grow bw2 shadow-5'>
             <h1>RoboFriends</h1>
             <div>
                 <img src={`https://robohash.org/${id}?200x200`}alt='robots'/>
                 <div>
                     <h2>{name}</h2>
-                    <p>{email}</p>
+                    <h2>{email}</h2>
                 </div>
             </div>
         </div>
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px' }}>
+        <div style={{ overflow: 'scroll', border: '5px solid #2EA0D6', height: '800px' }}>
             {props.children}
         </div>
     );
